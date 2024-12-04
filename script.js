5ca27065fd64b40da16467e2d7687e99ecbec4d9
Olha Kondyukh <kondyukh.olya@gmail.com>
2024-12-04 20:47:44 +0100

    add clean button and calculations display

--- script.js
+++ script.js
@@ -1,48 +1,82 @@
-const p = document.querySelector("p");
-let numbersArr = [[], []];
-let index = 0;
-let operatorValue = "";
-
+const outputContent = document.getElementById("output_content");
+const counting = document.getElementById("counting");
 const numbers = document.querySelectorAll(".number");
 const operators = document.querySelectorAll(".operator");
 const equal = document.getElementById("equal");
 const reset = document.getElementById("reset");
+const clean = document.getElementById("clean");
 
-function calculations() {
+let numbersArr = [[], []];
+let index = 0;
+let operatorValue = "";
+let display = [];
+
+function calculations(event) {
   let first = Number(numbersArr[0].join(""));
   let second = Number(numbersArr[1].join(""));
+  let res;
+
+  if (display.length < 3) {
+    display.push(numbersArr[index]);
+  }
+  counting.innerHTML = display.join("");
   console.log(numbersArr);
+  console.log(`display ${display}`);
   switch (operatorValue) {
     case "%":
       first = ((100 * second) / first).toFixed(2);
       break;
     case "+":
-      first += second;
+      res = first + second;
       break;
     case "-":
-      first -= second;
+      res = first - second;
       break;
     case "*":
-      first *= second;
+      res = first * second;
       break;
     case "/":
-      first /= second;
+      res = first / second;
       break;
     default:
     //first = [];
   }
-  first = first.toString();
-  p.innerHTML = first;
-  numbersArr[0] = [first];
+  res = res.toString();
+  outputContent.innerHTML = `=${res}`;
+  numbersArr[0] = [res];
   numbersArr[1] = [];
   operatorValue = "";
+  try {
+    if (event.target.value === "=") {
+      counting.innerHTML = res;
+      display = [res];
+    }
+  } catch {
+    //
+  }
 }
 
+//clean all previous calculations
 function resetCalc() {
   numbersArr = [[], []];
   index = 0;
   operatorValue = "";
-  p.innerHTML = 0;
+  outputContent.innerHTML = 0;
+  counting.innerHTML = "";
+  display = [];
+}
+
+//delete the last element from the number arr
+function cleanArr() {
+  if (numbersArr !== undefined) {
+    numbersArr[index].pop();
+    display.pop();
+    counting.innerHTML = display.join("");
+    outputContent.innerHTML = numbersArr[index].join("");
+    if (numbersArr[index].length === 0) {
+      outputContent.innerHTML = 0;
+    }
+  }
 }
 
 // add event listener for numbers
@@ -50,14 +84,19 @@ numbers.forEach(function (number) {
   number.addEventListener("click", function (event) {
     numbersArr[index].push(event.target.value);
     if (index === 1) {
+      //check if was tapped operator after adding second number
       if (numbersArr[[0]].length !== 0 && operatorValue == "") {
         numbersArr[[0]] = [event.target.value];
         numbersArr[[1]] = [];
         index = 0;
       }
     }
+    display.push(event.target.value);
+    counting.innerHTML = display.join("");
+    outputContent.innerHTML = numbersArr[index].join("");
+
     console.log(numbersArr);
-    p.innerHTML = numbersArr[index].join("");
+    console.log(`display ${display}`);
   });
 });
 
@@ -69,9 +108,12 @@ operators.forEach(function (operator) {
       calculations();
     }
     operatorValue = event.target.value;
-    console.log(operatorValue);
+    display.push(operatorValue);
+    counting.innerHTML = display.join("");
     index = 1;
-    p.innerHTML = operatorValue;
+
+    console.log(operatorValue);
+    console.log(`display [${display}]`);
   });
 });
 
@@ -80,3 +122,6 @@ equal.addEventListener("click", calculations);
 
 // add event listener for reset button
 reset.addEventListener("click", resetCalc);
+
+// add event listener for clean button
+clean.addEventListener("click", cleanArr);
